714396c382e5e3e92ad96d1860397646903c55d7
Jesus Marenco <jesusmarencodev@gmail.com>
2023-07-24 13:01:12 -0500

    signals part 2

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-properties-page',
   templateUrl: './properties-page.component.html',
-  styleUrls: ['./properties-page.component.css']
+  styleUrls: ['./properties-page.component.css'],
 })
 export class PropertiesPageComponent {
+  public user = signal<User>({
+    id: 2,
+    email: 'janet.weaver@reqres.in',
+    first_name: 'Janet',
+    last_name: 'Weaver',
+    avatar: 'https://reqres.in/img/faces/2-image.jpg',
+  });
 
+  onFieldUpdated(field: keyof User, value: string) {
+    //Way update
+    this.user.update((current) => ({
+      ...current,
+      [field]: value,
+    }));
+
+    //Way Mutate
+    /*     this.user.mutate(current =>{
+      switch(field){
+        case 'email':
+          current.email = value;
+        break;
+        case 'first_name':
+          current.first_name = value;
+        break;
+        case 'last_name':
+          current.last_name = value;
+        break;
+      }
+    }) */
+  }
 }
--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
+import { UsersServiceService } from '../../services/users-service.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-user-ingo-page',
   templateUrl: './user-info-page.component.html',
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
+  private usersServiceService = inject(UsersServiceService);
+  public userId = signal(1);
+  public currentUser = signal<User | undefined>(undefined);
+  public userWasFound = signal(true);
 
+  public fullName = computed<string>(() => {
+    if (!this.currentUser) return 'User not found';
+    return `${this.currentUser()?.first_name} ${this.currentUser()?.last_name}`;
+  });
+
+  ngOnInit(): void {
+    this.loadUser(this.userId());
+  }
+
+  loadUser(id: number) {
+    if (id <= 0) return;
+
+    this.userId.set(id);
+    this.currentUser.set(undefined);
+
+    this.usersServiceService.gerUserById(id).subscribe({
+      next: (user) => {
+        this.currentUser.set(user);
+        this.userWasFound.set(true);
+      },
+      error: () => this.userWasFound.set(false),
+    });
+  }
 }
